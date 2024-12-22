b50de2aee953cc6ee0548dcdf9db079b0e935bc5
Jari Ylimäinen <jari@ylimainen.fi>
2024-12-22 12:39:53 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-function parsePodcastLink(link) {
-  const re = /areena\.yle\.fi\/podcastit\/([\d-]+)/;
-  const found = link.match(re)
+function parseAreenaLink(event) {
+  const add_antennapod = document.getElementById("add_antennapod")
+  const target = event.explicitOriginalTarget || event.currentTarget || event.target
+  const re = /areena\.yle\.fi\/[\S\/]*(\d-[\d]+)/;
+  const found = target.value.trim().match(re)
 
-  return found[1]
+  if (found) {
+    const podcastId = found[1]
+    const rssUrl = `https://feeds.yle.fi/areena/v1/series/${podcastId}.rss`
+    subscribeLink = `https://antennapod.org/deeplink/subscribe/?url=${encodeURIComponent(rssUrl)}`
+
+    add_antennapod.onclick = () => {
+      // window.location.href = subscribeLink
+
+const fetchPromise = fetch(rssUrl);
+
+fetchPromise
+  .then((response) => {console.log(response.status)})
+
+    }
+
+    add_antennapod.classList.remove('invisible');
+  } else {
+    add_antennapod.classList.add('invisible');
+  }
 }
